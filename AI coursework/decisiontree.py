83242efb4fbd3bc2cdd09908682893d967dfe5c0
MarcTatam <marc.smsjb.tatam@gmail.com>
2021-03-22 22:03:48 +0000

    Data import

--- AI coursework/decisiontree.py
+++ AI coursework/decisiontree.py
@@ -1,3 +1,18 @@
 import csv
 
 def get_data()->list:
+    """Reads data from the csv file
+    
+    Returns
+    list of each row"""
+    return_list = []
+    csvfile = open("data.csv")
+    csvreader = csv.reader(csvfile)
+    return_list = []
+    for line in csvreader:
+        return_list.append(line)
+    csvfile.close
+    return return_list
+
+if __name__ == "__main__":
+    print(len(get_data()))
