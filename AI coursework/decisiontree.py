1964fcd632581a9e4200fcccb356932e394b0950
MarcTatam <marc.smsjb.tatam@gmail.com>
2021-03-23 20:06:56 +0000

    Finish algorithm first_draft

--- AI coursework/decisiontree.py
+++ AI coursework/decisiontree.py
@@ -1,21 +1,10 @@
 from decisiontreedata import get_data, sort_data
 from collections import Counter
+from math import log2
 class Attribute():
     def __init__(self, value, equailty):
         self.value = value
         self.equality = equailty
-        self.under_eighteen_passed = 0
-        self.eighteen_to_thirty_passed = 0
-        self.thirty_to_fourty_passed = 0
-        self.fourty_to_fifty_passed = 0
-        self.fifty_to_sixty_passed = 0
-        self.sixty_plus_passed = 0
-        self.under_eighteen_failed = 0
-        self.eighteen_to_thirty_failed = 0
-        self.thirty_to_fourty_failed = 0
-        self.fourty_to_fifty_failed = 0
-        self.fifty_to_sixty_failed = 0
-        self.sixty_plus_failed = 0
         self.childTrue = None
         self.childFalse = None
         self.parent = None
@@ -30,34 +19,6 @@ class Attribute():
             return self.value < other
         return False
 
-    def training_passed(self, other, classification):
-        if passed(self, other):
-            if classification == "age_under_18":
-                self.under_eighteen_passed += 1
-            elif classification == "age_18_30":
-                self.eighteen_to_thirty_passed += 1
-            elif classification == "age_30_40":
-                self.thirty_to_fourty_passed += 1
-            elif classification == "age_40_50":
-                self.fourty_to_fifty_passed += 1
-            elif classification == "age_50_60":
-                self.fifty_to_sixty_passed +=1
-            elif classification == "age_over_60":
-                self.sixty_plus_passed += 1
-        else:
-            if classification == "age_under_18":
-                self.under_eighteen_failed += 1
-            elif classification == "age_18_30":
-                self.eighteen_to_thirty_failed += 1
-            elif classification == "age_30_40":
-                self.thirty_to_fourty_failed += 1
-            elif classification == "age_40_50":
-                self.fourty_to_fifty_failed += 1
-            elif classification == "age_50_60":
-                self.fifty_to_sixty_failed +=1
-            elif classification == "age_over_60":
-                self.sixty_plus_failed += 1
-
     def __eq__(self, other):
         return self.value == other.value and self.equality == other.equality
 
@@ -71,6 +32,56 @@ def get_mode(examples:list)->str:
         classification_list.append(item[21])
     counted_classifications = Counter(classification_list)
     return counted_classifications.most_common(1)[0][0]
+
+def entropy_calculation(dataset:list)->float:
+    """Calculates the entropy of a given list
+
+    Args
+    dataset - List of dataset to calculate entropy for
+
+    Returns
+    float - float value of the entropy of list"""
+    list_length = len(dataset)
+    counted_list = Counter(dataset)
+    entropy = 0
+    for count in counted_list.most_common(5):
+        if count[1] != 0:
+            entropy += (count[1]/list_length)*log2(count[1]/list_length)
+    return -entropy
+
+def calculate_ig(start_list:list, true_list:list, false_list:list)->float:
+    """Calculates the information gain from a set of data
+
+    Args
+    start_list - list before it is split
+    true_lsit - list of items that ended up being true
+    false_list - list of items that ended up being false
+    
+    Returns
+    float - float representing information gain of this data"""
+    start_entropy = entropy_calculation(start_list)
+    true_entropy = entropy_calculation(true_list)
+    false_entropy = entropy_calculation(false_list)
+    ig = start_entropy - ((len(true_list)/len(start_list))*true_entropy+(len(false_list)/len(start_list))*false_entropy)
+    return ig
+
+def choose_attribute(examples, attributes):
+    current_best_ig = 0
+    current_best_index = 0
+    for i in range(0,len(attributes)):
+        current_true = []
+        current_false = []
+        current_attribute = attributes[i]
+        for item in examples:
+            if current_attribute.passed(item[current_attribute.index]):
+                current_true.append(item[21])
+            else:
+                current_fales.append(item[21])
+        temp_ig = calculate_ig(examples, current_true, current_false)
+        if temp_ig > current_best_ig:
+            current_best_ig = temp_ig
+            current_best_index = i
+    return attributes[current_best_index]
 
 def DTL(examples:list, attributes:list, default:str)->DecisionTree:
     if len(examples == 0):
@@ -110,4 +121,15 @@ def DTL(examples:list, attributes:list, default:str)->DecisionTree:
     tree = DecisionTree(best_attribute)
     return tree
 
+def setup_attributes()->list:
+    """Function which creates the list of attributes. Created to make code more readable
+    
+    Returns
+    A list of all the attributes to use in the decision tree"""
+    R_intersections  = Attribute(0, "greater than")
+    R_
 
+
+if __name__ == "__main__":
+    print(entropy_calculation(["b","b","b","b","b","g","g","g","g","g"]))
+    print(calculate_ig(["b","b","b","b","b","g","g","g","g","g"],["b","g","g","g","g","g"],["b","b","b","b"]))
